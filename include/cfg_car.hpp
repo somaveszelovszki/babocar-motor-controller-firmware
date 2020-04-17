44f14bf4c34c934a086211c020f40e776c793231
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-04-17 22:18:04 +0200

    temp

---
+++ include/cfg_car.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <micro/utils/units.hpp>
+
+namespace cfg {
+
+constexpr micro::micrometer_t ENCODER_INCR_DISTANCE = micro::micrometer_t(10);
+constexpr float DC_MOTOR_MAX_DUTY = 0.92f;
+
+} // namespace cfg
--- src/ControlTask.cpp
+++ src/ControlTask.cpp
@@ -1,4 +1,9 @@
+#include <micro/hw/DC_Motor.hpp>
+#include <micro/hw/Encoder.hpp>
+#include <micro/control/PID_Controller.hpp>
+
 #include <cfg_board.h>
+#include <cfg_car.hpp>
 #include <globals.hpp>
 #include <RemoteControllerData.hpp>
 
@@ -12,6 +17,11 @@ extern QueueHandle_t remoteControllerQueue;
 
 namespace {
 
+hw::DC_Motor dcMotor(tim_DC_Motor, timChnl_DC_Motor_Bridge1, timChnl_DC_Motor_Bridge2, cfg::DC_MOTOR_MAX_DUTY);
+hw::Encoder encoder(tim_Encoder);
+PID_Controller speedCtrl(millisecond_t Ts, millisecond_t Ti, millisecond_t Td, float Kc, -cfg::DC_MOTOR_MAX_DUTY, cfg::DC_MOTOR_MAX_DUTY)
+
+
 } // namespace
 
 extern "C" void runControlTask(void) {
@@ -24,11 +34,13 @@ extern "C" void runControlTask(void) {
 }
 
 void tim_ControlLoop_PeriodElapsedCallback() {
-//    encoder_update((encoder_t*)&encoder);
-//    speed_measured_mps = encoder.last_diff / ENCODER_INCR_PER_MM / (ENCODER_PERIOD_US / 1000.0f);
-//    distance_mm = encoder.num_incr / ENCODER_INCR_PER_MM;
-//
-//    pi_controller_update((pi_controller_t*)&speedCtrl, speed_measured_mps);
-//    //dc_motor_write(speedCtrl.output);
-//    dc_motor_write(0.0f);
+
+    encoder.update();
+
+    speed_measured_mps = encoder.last_diff / ENCODER_INCR_PER_MM / (ENCODER_PERIOD_US / 1000.0f);
+    distance_mm = encoder.num_incr / ENCODER_INCR_PER_MM;
+
+    pi_controller_update((pi_controller_t*)&speedCtrl, speed_measured_mps);
+    //dc_motor_write(speedCtrl.output);
+    dc_motor_write(0.0f);
 }

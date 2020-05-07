1ae7a26c9a2773be57f12ecb05ed3bd398ada8d9
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-07 21:34:41 +0200

    ControlTask handles CAN messages

--- src/ControlTask.cpp
+++ src/ControlTask.cpp
@@ -2,12 +2,13 @@
 #include <micro/hw/Encoder.hpp>
 #include <micro/hw/SteeringServo.hpp>
 #include <micro/control/PID_Controller.hpp>
+#include <micro/panel/CanManager.hpp>
 #include <micro/port/task.hpp>
 #include <micro/utils/timer.hpp>
+#include <micro/utils/CarProps.hpp>
 
 #include <cfg_board.h>
 #include <cfg_car.hpp>
-#include <globals.hpp>
 #include <LateralControl.hpp>
 #include <LongitudinalControl.hpp>
 #include <RemoteControllerData.hpp>
@@ -18,60 +19,101 @@
 
 using namespace micro;
 
-extern QueueHandle_t lateralControlQueue;
-extern QueueHandle_t longitudinalControlQueue;
 extern QueueHandle_t remoteControllerQueue;
+
+CanManager vehicleCanManager(can_Vehicle, canRxFifo_Vehicle, millisecond_t(50));
 
 namespace {
 
+bool useSafetyEnableSignal         = true;
+micro::CarProps car                = micro::CarProps();
+micro::radian_t frontWheelOffset   = micro::radian_t(0);
+micro::radian_t frontWheelMaxDelta = micro::radian_t(0);
+micro::radian_t rearWheelOffset    = micro::radian_t(0);
+micro::radian_t rearWheelMaxDelta  = micro::radian_t(0);
+micro::radian_t extraServoOffset   = micro::radian_t(0);
+micro::radian_t extraServoMaxDelta = micro::radian_t(0);
+
+float MotorCtrl_P                  = 0.0f; // TODO
+float MotorCtrl_I                  = 0.0f;
+float MotorCtrl_D                  = 0.0f;
+float MotorCtrl_integralMax        = 4.0f;
+
 hw::DC_Motor dcMotor(tim_DC_Motor, timChnl_DC_Motor_Bridge1, timChnl_DC_Motor_Bridge2, cfg::MOTOR_MAX_DUTY);
 hw::Encoder encoder(tim_Encoder);
-PID_Controller speedCtrl(globals::MotorCtrl_P, globals::MotorCtrl_I, globals::MotorCtrl_D, globals::MotorCtrl_integralMax, -cfg::MOTOR_MAX_DUTY, cfg::MOTOR_MAX_DUTY, 0.01f);
+PID_Controller speedCtrl(MotorCtrl_P, MotorCtrl_I, MotorCtrl_D, MotorCtrl_integralMax, -cfg::MOTOR_MAX_DUTY, cfg::MOTOR_MAX_DUTY, 0.01f);
 
 hw::SteeringServo frontSteeringServo(tim_SteeringServo, timChnl_FrontSteeringServo, cfg::FRONT_STEERING_SERVO_PWM0, cfg::FRONT_STEERING_SERVO_PWM180,
-    cfg::SERVO_MAX_ANGULAR_VELO, globals::frontWheelOffset, globals::frontWheelMaxDelta, cfg::SERVO_WHEEL_TRANSFER_RATE);
+    cfg::SERVO_MAX_ANGULAR_VELO, frontWheelOffset, frontWheelMaxDelta, cfg::SERVO_WHEEL_TRANSFER_RATE);
 
 hw::SteeringServo rearSteeringServo(tim_SteeringServo, timChnl_RearSteeringServo, cfg::REAR_STEERING_SERVO_PWM0, cfg::REAR_STEERING_SERVO_PWM180,
-    cfg::SERVO_MAX_ANGULAR_VELO, globals::rearWheelOffset, globals::rearWheelMaxDelta, cfg::SERVO_WHEEL_TRANSFER_RATE);
+    cfg::SERVO_MAX_ANGULAR_VELO, rearWheelOffset, rearWheelMaxDelta, cfg::SERVO_WHEEL_TRANSFER_RATE);
 
 } // namespace
 
 extern "C" void runControlTask(void) {
 
-    micro::waitReady(lateralControlQueue);
-    micro::waitReady(longitudinalControlQueue);
     micro::waitReady(remoteControllerQueue);
 
-    LateralControl lateralControl;
-    LongitudinalControl longitudinalControl;
     RemoteControllerData remoteControllerData;
+
+    struct {
+        micro::radian_t frontWheelAngle;
+        micro::radian_t rearWheelAngle;
+        micro::radian_t extraServoAngle;
+    } lateralControl;
+
+    struct {
+        micro::m_per_sec_t speed;
+        micro::millisecond_t rampTime;
+    } longitudinalControl;
 
     struct {
         m_per_sec_t startSpeed, targetSpeed;
         microsecond_t startTime, duration;
     } speedRamp;
 
-    WatchdogTimer lateralWd(millisecond_t(50)), longitudinalWd(millisecond_t(50)), remoteControlWd(millisecond_t(50));
+    canFrame_t rxCanFrame;
+    CanFrameHandler vehicleCanFrameHandler;
+
+    vehicleCanFrameHandler.registerHandler(can::LateralControl::id(), [&lateralControl] (const uint8_t * const data) {
+        reinterpret_cast<const can::LateralControl*>(data)->acquire(lateralControl.frontWheelAngle, lateralControl.rearWheelAngle, lateralControl.extraServoAngle);
+    });
+
+    vehicleCanFrameHandler.registerHandler(can::LongitudinalControl::id(), [&longitudinalControl] (const uint8_t * const data) {
+        LongitudinalControl longitudinal;
+        reinterpret_cast<const can::LongitudinalControl*>(data)->acquire(longitudinalControl.speed, useSafetyEnableSignal, longitudinalControl.rampTime);
+    });
+
+    vehicleCanFrameHandler.registerHandler(can::SetFrontWheelParams::id(), [] (const uint8_t * const data) {
+        reinterpret_cast<const can::SetFrontWheelParams*>(data)->acquire(frontWheelOffset, frontWheelMaxDelta);
+        vehicleCanManager.send(can::FrontWheelParams(frontWheelOffset, frontWheelMaxDelta));
+    });
+
+    vehicleCanFrameHandler.registerHandler(can::SetRearWheelParams::id(), [] (const uint8_t * const data) {
+        reinterpret_cast<const can::SetRearWheelParams*>(data)->acquire(rearWheelOffset, rearWheelMaxDelta);
+        vehicleCanManager.send(can::RearWheelParams(rearWheelOffset, rearWheelMaxDelta));
+    });
+
+    const CanManager::subscriberId_t vehicleCanSubsciberId = vehicleCanManager.registerSubscriber(vehicleCanFrameHandler.identifiers());
+
+    WatchdogTimer remoteControlWd(millisecond_t(50));
 
     while (true) {
-        globals::isControlTaskOk = !lateralWd.hasTimedOut() && !longitudinalWd.hasTimedOut() && !remoteControlWd.hasTimedOut();
+        bool isOk = !vehicleCanManager.hasRxTimedOut() && !remoteControlWd.hasTimedOut(); // TODO framework-wide solution for task isOk
 
-        if (xQueueReceive(lateralControlQueue, &lateralControl, 0)) {
-            lateralWd.reset();
-        }
-
-        if (xQueueReceive(longitudinalControlQueue, &longitudinalControl, 0)) {
-            longitudinalWd.reset();
+        if (vehicleCanManager.read(vehicleCanSubsciberId, rxCanFrame)) {
+            vehicleCanFrameHandler.handleFrame(rxCanFrame);
         }
 
         if (xQueueReceive(remoteControllerQueue, &remoteControllerData, 0)) {
             remoteControlWd.reset();
         }
 
-        frontSteeringServo.setWheelOffset(globals::frontWheelOffset);
-        frontSteeringServo.setWheelMaxDelta(globals::frontWheelMaxDelta);
-        rearSteeringServo.setWheelOffset(globals::rearWheelOffset);
-        rearSteeringServo.setWheelMaxDelta(globals::rearWheelMaxDelta);
+        frontSteeringServo.setWheelOffset(frontWheelOffset);
+        frontSteeringServo.setWheelMaxDelta(frontWheelMaxDelta);
+        rearSteeringServo.setWheelOffset(rearWheelOffset);
+        rearSteeringServo.setWheelMaxDelta(rearWheelMaxDelta);
 
         switch (remoteControllerData.activeChannel) {
 
@@ -85,9 +127,9 @@ extern "C" void runControlTask(void) {
             break;
 
         case RemoteControllerData::channel_t::SafetyEnable:
-            if (globals::isControlTaskOk && isBtw(remoteControllerData.acceleration, 0.5f, 1.0f)) {
+            if (isOk && isBtw(remoteControllerData.acceleration, 0.5f, 1.0f)) {
                 if (longitudinalControl.speed != speedRamp.targetSpeed || longitudinalControl.rampTime != speedRamp.duration) {
-                    speedRamp.startSpeed  = globals::car.speed;
+                    speedRamp.startSpeed  = car.speed;
                     speedRamp.targetSpeed = longitudinalControl.speed;
                     speedRamp.startTime   = getExactTime();
                     speedRamp.duration    = longitudinalControl.rampTime;
@@ -97,7 +139,7 @@ extern "C" void runControlTask(void) {
                 rearSteeringServo.writeWheelAngle(lateralControl.rearWheelAngle);
             } else {
                 if (speedRamp.targetSpeed != m_per_sec_t(0) || speedRamp.duration != cfg::EMERGENCY_BRAKE_DURATION) {
-                    speedRamp.startSpeed  = globals::car.speed;
+                    speedRamp.startSpeed  = car.speed;
                     speedRamp.targetSpeed = m_per_sec_t(0);
                     speedRamp.startTime   = getExactTime();
                     speedRamp.duration    = cfg::EMERGENCY_BRAKE_DURATION;
@@ -114,8 +156,8 @@ extern "C" void runControlTask(void) {
 
         speedCtrl.desired = map(getExactTime(), speedRamp.startTime, speedRamp.startTime + speedRamp.duration, speedRamp.startSpeed, speedRamp.targetSpeed).get();
 
-        globals::car.frontWheelAngle = frontSteeringServo.wheelAngle();
-        globals::car.rearWheelAngle  = rearSteeringServo.wheelAngle();
+        car.frontWheelAngle = frontSteeringServo.wheelAngle();
+        car.rearWheelAngle  = rearSteeringServo.wheelAngle();
 
         vTaskDelay(1);
     }
@@ -130,11 +172,15 @@ void tim_ControlLoop_PeriodElapsedCallback() {
     const millisecond_t now = getExactTime();
     encoder.update();
 
-    globals::car.speed = encoder.lastDiff() * cfg::ENCODER_INCR_DISTANCE / (now - lastUpdateTime);
-    globals::car.distance = encoder.numIncr() * cfg::ENCODER_INCR_DISTANCE;
+    car.speed = encoder.lastDiff() * cfg::ENCODER_INCR_DISTANCE / (now - lastUpdateTime);
+    car.distance = encoder.numIncr() * cfg::ENCODER_INCR_DISTANCE;
 
-    speedCtrl.update(globals::car.speed.get());
+    speedCtrl.update(car.speed.get());
     dcMotor.write(speedCtrl.output());
 
     lastUpdateTime = now;
+}
+
+void micro_Vehicle_Can_RxFifoMsgPendingCallback() {
+    vehicleCanManager.onFrameReceived();
 }

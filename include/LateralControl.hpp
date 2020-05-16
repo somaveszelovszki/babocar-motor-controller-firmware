08681918e2923986018a21a4204f1922c078d994
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-16 12:53:07 +0200

    unused LateralControl.hpp and LongitudinalControl.hpp removed (structs defined in ControlTask)

--- include/LateralControl.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <micro/utils/units.hpp>
-
-struct LateralControl {
-    micro::radian_t frontWheelAngle;
-    micro::radian_t rearWheelAngle;
-    micro::radian_t extraServoAngle;
-};
--- include/LongitudinalControl.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <micro/utils/units.hpp>
-
-struct LongitudinalControl {
-    micro::m_per_sec_t speed;
-    micro::millisecond_t rampTime;
-};

fe77dd283b254360ca06f4505a35aca30f33cc61
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-10-18 22:39:31 +0200

    motor max duty reduced, servo 0-180 degree PWM values updated (not configured yet)

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -5,15 +5,15 @@
 namespace cfg {
 
 constexpr micro::micrometer_t ENCODER_INCR_DISTANCE     = micro::micrometer_t(10);
-constexpr float MOTOR_MAX_DUTY                          = 0.95f;
+constexpr float MOTOR_MAX_DUTY                          = 0.75f;
 constexpr micro::m_per_sec_t DIRECT_CONTROL_MAX_SPEED   = micro::m_per_sec_t(3);
 
 constexpr float SERVO_WHEEL_TRANSFER_RATE               = 1.0f;
 constexpr micro::rad_per_sec_t SERVO_MAX_ANGULAR_VELO   = micro::deg_per_sec_t(500);
-constexpr uint32_t FRONT_STEERING_SERVO_PWM0            = 500;
-constexpr uint32_t FRONT_STEERING_SERVO_PWM180          = 2500;
-constexpr uint32_t REAR_STEERING_SERVO_PWM0             = 500;
-constexpr uint32_t REAR_STEERING_SERVO_PWM180           = 2500;
+constexpr uint32_t FRONT_STEERING_SERVO_PWM0            = 5000;
+constexpr uint32_t FRONT_STEERING_SERVO_PWM180          = 25000;
+constexpr uint32_t REAR_STEERING_SERVO_PWM0             = 5000;
+constexpr uint32_t REAR_STEERING_SERVO_PWM180           = 25000;
 
 constexpr micro::millisecond_t EMERGENCY_BRAKE_DURATION = micro::millisecond_t(700);
 

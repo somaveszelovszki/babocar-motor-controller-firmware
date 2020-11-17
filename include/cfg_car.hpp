95006c339096aafb00243a8c6992b230e1a42d6a
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-11-17 16:50:10 +0100

    maximum wheel delta angle increased to 26deg

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -10,12 +10,12 @@ constexpr micro::m_per_sec_t DIRECT_CONTROL_MAX_SPEED          = micro::m_per_se
 
 constexpr uint32_t FRONT_STEERING_SERVO_PWM_CENTER             = 14300;
 constexpr micro::radian_t FRONT_STEERING_SERVO_TRANSFER_RATE   = micro::milliradian_t(0.12f);
-constexpr micro::radian_t FRONT_WHEEL_MAX_DELTA_ANGLE          = micro::degree_t(24);
+constexpr micro::radian_t FRONT_WHEEL_MAX_DELTA_ANGLE          = micro::degree_t(26);
 constexpr micro::rad_per_sec_t FRONT_SERVO_MAX_ANGULAR_VELO    = micro::deg_per_sec_t(500);
 
 constexpr uint32_t REAR_STEERING_SERVO_PWM_CENTER              = 14700;
 constexpr micro::radian_t REAR_STEERING_SERVO_TRANSFER_RATE    = micro::milliradian_t(0.13f);
-constexpr micro::radian_t REAR_WHEEL_MAX_DELTA_ANGLE           = micro::degree_t(24);
+constexpr micro::radian_t REAR_WHEEL_MAX_DELTA_ANGLE           = micro::degree_t(26);
 constexpr micro::rad_per_sec_t REAR_SERVO_MAX_ANGULAR_VELO     = micro::deg_per_sec_t(500);
 
 constexpr micro::millisecond_t EMERGENCY_BRAKE_DURATION_HARD   = micro::millisecond_t(200);

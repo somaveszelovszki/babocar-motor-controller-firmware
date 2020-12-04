8d72c901690adc5aafe80ddc8192efdfbd886851
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-12-04 16:21:23 +0100

    motor max duty reduced to 85% to prevent overcurrent (for now :D)

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -5,7 +5,7 @@
 namespace cfg {
 
 constexpr micro::micrometer_t ENCODER_INCR_DISTANCE                    = micro::micrometer_t(8.16573169f); // 122463 ticks per 1 meter
-constexpr float MOTOR_MAX_DUTY                                         = 0.92f;
+constexpr float MOTOR_MAX_DUTY                                         = 0.85f;
 constexpr micro::m_per_sec_t DIRECT_CONTROL_MAX_SPEED                  = micro::m_per_sec_t(3);
 
 constexpr uint32_t FRONT_STEERING_SERVO_PWM_CENTER                    = 14300;

05d8112b550b0fd9541c9232906783becd75dc40
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-12-13 18:32:58 +0100

    servo motor offsets and transfer rates updated

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -9,14 +9,14 @@ constexpr float MOTOR_MAX_DUTY                                         = 0.85f;
 constexpr micro::m_per_sec_t DIRECT_CONTROL_MAX_SPEED                  = micro::m_per_sec_t(1.7f);
 
 constexpr uint32_t FRONT_STEERING_SERVO_PWM_CENTER                    = 14300;
-constexpr micro::radian_t FRONT_STEERING_SERVO_POSITIVE_TRANSFER_RATE = micro::milliradian_t(0.122f);
-constexpr micro::radian_t FRONT_STEERING_SERVO_NEGATIVE_TRANSFER_RATE = micro::milliradian_t(0.114f);
+constexpr micro::radian_t FRONT_STEERING_SERVO_POSITIVE_TRANSFER_RATE = micro::milliradian_t(0.12965f);
+constexpr micro::radian_t FRONT_STEERING_SERVO_NEGATIVE_TRANSFER_RATE = micro::milliradian_t(0.12979f);
 constexpr micro::radian_t FRONT_WHEEL_MAX_DELTA_ANGLE                 = micro::degree_t(26);
 constexpr micro::rad_per_sec_t FRONT_SERVO_MAX_ANGULAR_VELOCITY       = micro::deg_per_sec_t(500);
 
 constexpr uint32_t REAR_STEERING_SERVO_PWM_CENTER                     = 14340;
-constexpr micro::radian_t REAR_STEERING_SERVO_POSITIVE_TRANSFER_RATE  = micro::milliradian_t(0.130f);
-constexpr micro::radian_t REAR_STEERING_SERVO_NEGATIVE_TRANSFER_RATE  = micro::milliradian_t(0.130f);
+constexpr micro::radian_t REAR_STEERING_SERVO_POSITIVE_TRANSFER_RATE  = micro::milliradian_t(0.12901f);
+constexpr micro::radian_t REAR_STEERING_SERVO_NEGATIVE_TRANSFER_RATE  = micro::milliradian_t(0.12779f);
 constexpr micro::radian_t REAR_WHEEL_MAX_DELTA_ANGLE                  = micro::degree_t(26);
 constexpr micro::rad_per_sec_t REAR_SERVO_MAX_ANGULAR_VELOCITY        = micro::deg_per_sec_t(500);
 

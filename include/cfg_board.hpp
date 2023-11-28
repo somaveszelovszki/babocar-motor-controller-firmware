4d556bfcba4e7f54130043eb7c25cabb1b24865b
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-11-28 21:36:51 +0100

    Windows build errors fixed

--- include/cfg_board.hpp
+++ include/cfg_board.hpp
@@ -5,6 +5,8 @@
 #include <micro/port/timer.hpp>
 #include <micro/port/uart.hpp>
 
+#if defined STM32F4
+
 extern CAN_HandleTypeDef  hcan1;
 extern TIM_HandleTypeDef  htim1;
 extern TIM_HandleTypeDef  htim2;
@@ -44,6 +46,40 @@ extern UART_HandleTypeDef huart2;
 #define timChnl_RcRecv2             TIM_CHANNEL_2
 
 #define uart_Debug                  micro::uart_t{ &huart2 }
+
+#else // !STM32F4
+
+#define can_Vehicle                 micro::can_t{}
+
+#define gpio_Led                    micro::gpio_t{}
+
+#define tim_DC_Motor                micro::timer_t{}
+#define timChnl_DC_Motor_Bridge1    1
+#define timChnl_DC_Motor_Bridge2    2
+
+#define tim_System                  micro::timer_t{}
+
+#define tim_SteeringServo           micro::timer_t{}
+#define timChnl_FrontSteeringServo  1
+#define timChnl_RearSteeringServo   2
+
+#define tim_ServoX                  micro::timer_t{}
+#define timChnl_ServoX              0
+
+#define tim_RcCtrl                  micro::timer_t{}
+#define timChnl_RcCtrlSteer         4
+#define timChnl_RcCtrlAccel         3
+#define timChnl_RcCtrlModeSelect    2
+
+#define tim_ControlLoop             micro::timer_t{}
+
+#define tim_Encoder                 micro::timer_t{}
+#define timChnl_RcRecv1             1
+#define timChnl_RcRecv2             2
+
+#define uart_Debug                  micro::uart_t{}
+
+#endif // !STM32F4
 
 #define PANEL_VERSION               0x02
 

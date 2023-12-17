63e77d5bc5da4e1b909adf33d323e619d6e4e28c
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2023-12-17 22:33:10 +0100

    Rear steering servo center position reconfigured

--- include/cfg_car.hpp
+++ include/cfg_car.hpp
@@ -14,7 +14,7 @@ constexpr micro::radian_t FRONT_STEERING_SERVO_NEGATIVE_TRANSFER_RATE = micro::m
 constexpr micro::radian_t FRONT_WHEEL_MAX_DELTA_ANGLE                 = micro::degree_t(26);
 constexpr micro::rad_per_sec_t FRONT_SERVO_MAX_ANGULAR_VELOCITY       = micro::deg_per_sec_t(500);
 
-constexpr uint32_t REAR_STEERING_SERVO_PWM_CENTER                     = 14340;
+constexpr uint32_t REAR_STEERING_SERVO_PWM_CENTER                     = 14560;
 constexpr micro::radian_t REAR_STEERING_SERVO_POSITIVE_TRANSFER_RATE  = micro::milliradian_t(0.12901f);
 constexpr micro::radian_t REAR_STEERING_SERVO_NEGATIVE_TRANSFER_RATE  = micro::milliradian_t(0.12779f);
 constexpr micro::radian_t REAR_WHEEL_MAX_DELTA_ANGLE                  = micro::degree_t(26);
